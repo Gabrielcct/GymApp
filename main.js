f4a42f486b51702d51dfbe29536ecf10dd40bc95
Gabriel <2021240@student.cct.ie>
2023-05-21 20:39:27 +0100

    electron menu created and build

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const { app, BrowserWindow } = require("electron");
-// Node module(s).
+"use strict";
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const nodePath = require("path");
 let win = null;
 
+// WINDOW
 function createWindow() {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: 1000,
+    width: 1200,
     height: 1080,
+    min_width: 680,
+    min_height: 660,
     backgroundColor: "#ffffff",
     show: false,
     webPreferences: {
@@ -25,22 +30,15 @@ function createWindow() {
     win.show();
   });
 
-  //win.loadURL(`file:/${__dirname}/dist/index.html`);
-
   win.on("closed", function () {
     win = null;
   });
 }
 
+// APP
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
-  }
-});
-
-app.on("activate", function () {
-  if (win == null) {
-    createWindow();
   }
 });
 
@@ -49,3 +47,76 @@ app.on("ready", () => {
     createWindow();
   }
 });
+// MENU
+const { Menu, MenuItem } = require("electron");
+
+const template = [
+  {
+    label: "File",
+    submenu: [
+      {
+        role: "quit",
+      },
+    ],
+  },
+  {
+    label: "Edit",
+    submenu: [
+      {
+        role: "undo",
+      },
+      {
+        role: "redo",
+      },
+      {
+        type: "separator",
+      },
+      {
+        role: "cut",
+      },
+      {
+        role: "copy",
+      },
+      {
+        role: "paste",
+      },
+    ],
+  },
+
+  {
+    label: "View",
+    submenu: [
+      {
+        role: "togglefullscreen",
+      },
+      {
+        role: "minimize",
+      },
+      {
+        type: "separator",
+      },
+      {
+        role: "zoomin",
+      },
+      {
+        role: "zoomout",
+      },
+      {
+        role: "resetzoom",
+      },
+    ],
+  },
+
+  {
+    label: "About GymFit",
+    submenu: [
+      {
+        role: "about",
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+
+Menu.setApplicationMenu(menu);
